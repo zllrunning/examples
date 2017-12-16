d9d367496a500b21298b53d98a072d2da5868347
zll <zllrunning@gmail.com>
2017-12-16 22:36:52 +0800

    Update vgg.py
    添加了models.vgg16(pretrained=True).features的结构，解决了我之前疑惑的地方

--- fast_neural_style/neural_style/vgg.py
+++ fast_neural_style/neural_style/vgg.py
@@ -13,7 +13,7 @@ class Vgg16(torch.nn.Module):
         self.slice3 = torch.nn.Sequential()
         self.slice4 = torch.nn.Sequential()
         for x in range(4):
-            self.slice1.add_module(str(x), vgg_pretrained_features[x])
+            self.slice1.add_module(str(x), vgg_pretrained_features[x])   #vgg_pretrained_features[x]在索引内部相应的层
         for x in range(4, 9):
             self.slice2.add_module(str(x), vgg_pretrained_features[x])
         for x in range(9, 16):
@@ -36,3 +36,50 @@ class Vgg16(torch.nn.Module):
         vgg_outputs = namedtuple("VggOutputs", ['relu1_2', 'relu2_2', 'relu3_3', 'relu4_3'])
         out = vgg_outputs(h_relu1_2, h_relu2_2, h_relu3_3, h_relu4_3)
         return out
+
+    
+#models.vgg16(pretrained=True).features的结构如下所示，
+Sequential (
+  (0): Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+  (1): ReLU (inplace)
+  (2): Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+  (3): ReLU (inplace)
+  #  slice1  slice1为从0-3的网络
+    
+  (4): MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
+  (5): Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+  (6): ReLU (inplace)
+  (7): Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+  (8): ReLU (inplace)
+    #  slice2  slice2为从4-8的网络
+    
+  (9): MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
+  (10): Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+  (11): ReLU (inplace)
+  (12): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+  (13): ReLU (inplace)
+  (14): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+  (15): ReLU (inplace)
+    #  slice3  slice3为从9-15的网络
+    
+  (16): MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
+  (17): Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+  (18): ReLU (inplace)
+  (19): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+  (20): ReLU (inplace)
+  (21): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+  (22): ReLU (inplace)
+    #  slice4  slice4为从16-22的网络
+    
+  (23): MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
+  (24): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+  (25): ReLU (inplace)
+  (26): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+  (27): ReLU (inplace)
+  (28): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+  (29): ReLU (inplace)
+  (30): MaxPool2d (size=(2, 2), stride=(2, 2), dilation=(1, 1))
+)
+    
+    
+    
